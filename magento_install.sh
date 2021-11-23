e2a1f97ec91547c5a0bd87992f2740d559ff1920
Lakindu Akash <Lakinduakash@gmail.com>
2021-11-23 21:05:27 +0530

    Disable composer check

--- magento_install.sh
+++ magento_install.sh
@@ -127,7 +127,7 @@ apt-get install -yq php7.4-{mysql,mcrypt,gd,curl,intl,bcmath,ctype,dom,iconv,mbs
 echo "Intalling composer..."
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+# php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
